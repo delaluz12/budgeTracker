07a0b55762f19601cb7cda2eab2a4a1d6d805693
delaluz12 <75863526+delaluz12@users.noreply.github.com>
2021-11-02 19:56:10 -0500

    changes to added icons

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,7 +6,9 @@ const FILES_TO_CACHE = [
     "/styles.css",
     "/index.js",
     "/icons/icon-144x144.png",
-    "/icons/icon-384x384.png"
+    "/icons/icon-192x192.png",
+    "/icons/icon-384x384.png",
+    "/icons/icon-512x512.png"
 
 
 ];
